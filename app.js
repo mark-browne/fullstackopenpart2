969328de850be5120ee025de4620f3153e3d576b
mark browne <markbrowne789@hotmail.com>
2023-06-25 12:06:13 +0100

    ex 2.10 split components into files

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import Form from './form'
+import Filter from './filter'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -43,20 +45,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-          filter: <input value={newFilter} onChange={handleFilteredChange}/>
-        </div>
-      <form id="myform" onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter handleFilteredChange={handleFilteredChange}/>
+      <Form addPerson={addPerson} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person,i) => <li key={i}>{person.name + " " + person.number}</li>)}

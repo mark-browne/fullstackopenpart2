0d92f8215973432d78441063e3707d3187d2dccb
mark browne <markbrowne789@hotmail.com>
2023-06-25 11:40:36 +0100

    ex 2.9 filter

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',"number" :9999999 }
   ]) 
+
+  const [filteredPersons, setFilteredPersons] = useState([
+    { name: '' }
+  ]) 
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addPerson = (event)=> {
     const inlist = persons.filter(item => item.name === newName)
@@ -29,14 +35,17 @@ const App = () => {
     setNewNumber(event.target.value)
   } 
 
-  const Display = ()=>{
-    return (<div>{JSON.stringify(persons)}</div>)
-  }
-
+  const handleFilteredChange = (event)=> {
+    setNewFilter(event.target.value)
+    setFilteredPersons(persons.filter(item => item.name.includes(newFilter)))
+  } 
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter: <input value={newFilter} onChange={handleFilteredChange}/>
+        </div>
       <form id="myform" onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -50,9 +59,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person,i) => <li key={i}>{person.name , person.number}</li>)}
+        {persons.map((person,i) => <li key={i}>{person.name + " " + person.number}</li>)}
      </ul>
-     <Display/>
+      <ul>
+        {filteredPersons.map((person,i) => <li key={i}>{person.name + " " + person.number}</li>)}
+      </ul>  
      </div>
   )
 }
